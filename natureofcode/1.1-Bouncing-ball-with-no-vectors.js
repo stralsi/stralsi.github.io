674d1aa017f3b3722f375334e1d45c44eea749dd
Silviu Straliciuc <silviu.straliciuc@gmail.com>
2014-01-02 00:35:50 +0200

    switched to "Revealing module" pattern

--- natureofcode/1.1-Bouncing-ball-with-no-vectors.js
+++ natureofcode/1.1-Bouncing-ball-with-no-vectors.js
@@ -1,58 +1,61 @@
 var natureOfCode = natureOfCode || {};
 
-(function (natureOfCode) {
+
+natureOfCode.BouncingBallWithNoVectors = function (canvas) {
     "use strict";
-    natureOfCode.bouncingBallWithNoVectors = function (canvas) {
-        var x = 0,
-            y = 0,
-            width = 800,
-            height = 200,
-            ballRadius = 10,
-            ballColor = '#00ff00',
-            xspeed = 1,
-            yspeed = 3.3,
-            refreshRate = 40,
-            backgroundColor = '#eeeeee',
+    var x = 0,
+        y = 0,
+        width = 800,
+        height = 200,
+        ballRadius = 10,
+        ballColor = '#00ff00',
+        xspeed = 1,
+        yspeed = 3.3,
+        refreshRate = 40,
+        backgroundColor = '#eeeeee',
 
-            clear = function () {
-                var context = canvas.getContext('2d');
-                context.clearRect(0, 0, width, height);
-                context.fillStyle   = backgroundColor;
-                context.fillRect(0, 0, width, height);
-            },
+        clear = function () {
+            var context = canvas.getContext('2d');
+            context.clearRect(0, 0, width, height);
+            context.fillStyle   = backgroundColor;
+            context.fillRect(0, 0, width, height);
+        },
 
-            setup = function () {
-                canvas.width = width;
-                canvas.height = height;
-                clear(canvas);
-            },
+        setup = function () {
+            canvas.width = width;
+            canvas.height = height;
+            clear(canvas);
+        },
 
-            draw = function () {
-                clear(canvas);
+        draw = function () {
+            clear(canvas);
 
-                x = x + xspeed;
-                y = y + yspeed;
-                //console.log(x + ',' + y);
+            x = x + xspeed;
+            y = y + yspeed;
+            //console.log(x + ',' + y);
 
-                if ((x > width) || (x < 0)) {
-                    xspeed = xspeed * -1;
-                }
-                if ((y > height) || (y < 0)) {
-                    yspeed = yspeed * -1;
-                }
+            if ((x > width) || (x < 0)) {
+                xspeed = xspeed * -1;
+            }
+            if ((y > height) || (y < 0)) {
+                yspeed = yspeed * -1;
+            }
 
 
-                var context = canvas.getContext('2d');
+            var context = canvas.getContext('2d');
 
-                context.beginPath();
-                context.arc(x, y, ballRadius, 0, 2 * Math.PI, false);
-                context.fillStyle = ballColor;
-                context.fill();
+            context.beginPath();
+            context.arc(x, y, ballRadius, 0, 2 * Math.PI, false);
+            context.fillStyle = ballColor;
+            context.fill();
 
-                setTimeout(draw, refreshRate);
-            };
+            setTimeout(draw, refreshRate);
+        },
 
-        setup();
-        draw();
-    };
-}(natureOfCode));
+        run = function () {
+            setup();
+            draw();
+        };
+
+    return {run: run};
+};
